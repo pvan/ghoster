62381c1927da21df045add74bbfb9baf7bf94b43
Phil Vanderveen <philipvanderveen@gmail.com>
2017-11-15 21:07:01 -0500

    add quick launch dev enviroment

---
+++ launchdev.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set filesToOpen=ghoster.cpp
+
+
+if not defined DevEnvDir (
+    call "D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
+
+
+start "" "D:\Program Files\Sublime Text 3\sublime_text.exe" %filesToOpen%
+
+start "" "devenv" "build\ghoster.exe" %filesToOpen%
+
